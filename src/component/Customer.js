1d85fedcca52ca64b20c7b167dbb80aebfc4a9e1
suhyeoon <87221010+shjeonlab@users.noreply.github.com>
2021-10-04 01:05:10 +0900

    refactor: fix component

--- src/component/Customer.js
+++ src/component/Customer.js
@@ -1,45 +1,19 @@
 import React from 'react';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
 
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <CustomerProfile
-                    id={this.props.id}
-                    image={this.props.image}
-                    name={this.props.name}
-                />
-                <CustomerInfo
-                    phone={this.props.phone}
-                    area={this.props.area}
-                />
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.phone}</TableCell>
+                <TableCell>{this.props.area}</TableCell>
+            </TableRow>
         );
     }
 }
-
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        );
-    }
-}
-
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.phone}</p>
-                <p>{this.props.area}</p>
-            </div>
-        );
-    }
-}
-
 
 export default Customer;
