6595ed2639d087f650d162e7f940355ec6a12ea2
suhyeoon <87221010+shjeonlab@users.noreply.github.com>
2021-10-06 23:43:17 +0900

    feat: add a REST API

--- server.js
+++ server.js
@@ -6,8 +6,30 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '홍길동',
+            'phone': '010-1111-1111',
+            'area': '서울특별시'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '이순신',
+            'phone': '010-2222-2222',
+            'area': '경기도'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '이황',
+            'phone': '010-3333-3333',
+            'area': '부산광역시'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
