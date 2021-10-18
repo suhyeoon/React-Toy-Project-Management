b2fb608036a3df8af6a7708f45e1ee951f2959c8
suhyeoon <87221010+shjeonlab@users.noreply.github.com>
2021-10-18 23:39:56 +0900

    feat: add customer database table and database connection settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,30 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.porf,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'phone': '010-1111-1111',
-            'area': '서울특별시'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '이순신',
-            'phone': '010-2222-2222',
-            'area': '경기도'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이황',
-            'phone': '010-3333-3333',
-            'area': '부산광역시'
-        }
-    ]);
+
+    connection.query(
+        // query
+    );
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
